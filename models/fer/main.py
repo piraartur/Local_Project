4c50c3f57bde11f84627cc567f4d45c1d7bd2d7a
Sandpitturtleee <karolmazur@onet.eu>
2023-06-04 22:42:06 +0200

    Adding fer

--- models/fer/main.py
+++ models/fer/main.py
@@ -1,15 +1,10 @@
-import cv2
+import time
 
-from fer import FER
-
-import matplotlib.pyplot as plt
-
-import matplotlib.image as mpimg
+from models.fer.face_detection import fer
 
 if __name__ == "__main__":
-    input_image = cv2.imread(
-        "C:/Users/karol/Desktop/Studia/6 Semestr/ProjektLokalny/Local_Project/datasets/fer2013/test/angry/PrivateTest_3411628.jpg"
-    )
-    emotion_detector = FER()
-    # Output image's information
-    print(emotion_detector.detect_emotions(input_image))
+    start_time = time.time()
+
+    fer()
+
+    print("--- %s seconds ---" % (time.time() - start_time))
