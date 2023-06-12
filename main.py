9b7aa32065d6ef41af1b3da108c61992ea0a3a1a
Pawko23 <123464760+Pawko23@users.noreply.github.com>
2023-06-12 11:04:57 +0200

    Implemented UI for data representation
    * Added method to save results to CSV file
    
    * Adding fer
    
    * Fixing fer
    
    * Changes with Show All button
    
    * Added method to save csv files for each emotion with its accuracy; provided csv in the directory
    
    * Provided csv files ommited previously
    
    * Added 2nd graph and design changes
    
    * Calculated mean for each column in dataframe; fixed csv saving method
    
    * Added last graph, sticky functionality to Show All button, and deleted radio buttons
    
    * Added last graph, sticky functionality to Show All button, and deleted radio buttons
    
    ---------
    
    Co-authored-by: piraartur <piraartur@gmail.com>
    Co-authored-by: Sandpitturtleee <karolmazur@onet.eu>

--- main.py
+++ main.py
@@ -1,71 +1,75 @@
 import pandas as pd
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
+from dash import Dash, html, dcc, Output, Input
 import plotly.graph_objects as go
 
-df = pd.read_csv('models/deepface/csv_files/emotions_deepface.csv')
+deepfaceCSV = pd.read_csv('models/deepface/csv_files/emotions_deepface.csv')
+ferCSV = pd.read_csv('models/fer/csv_files/emotions_fer.csv')
+cnnCSV = pd.read_csv('models/CNN/csv_files/emotion_results.csv')
+
+column_names = deepfaceCSV.columns.tolist()
+options = [{'label': key, 'value': key} for key in column_names]
 
 app = Dash(__name__, assets_folder='assets')
 app.layout = html.Div([
     html.Div(children='Facial Expressions Results', className='title'),
     html.Hr(),
-    dcc.RadioItems(
-        options=[{'label': key, 'value': key} for key in df.columns],
-        value='',
-        id='controls-and-radio-item',
-        className='radio-buttons'
-    ),
+    html.Div(className='sticky-div', children=[
+        html.Button("Show All", id="show-all-btn", n_clicks=0, className="show-all-btn")
+    ]),
     dcc.Graph(figure={}, id='deepfaceModel', className='deepfaceModel'),
-    dcc.Graph(figure={}, id='ferModel', className='ferModel')
+    dcc.Graph(figure={}, id='ferModel', className='ferModel'),
+    dcc.Graph(figure={}, id='cnnModel', className='cnnModel')
 ])
 
-@callback(
-    [Output(component_id='deepfaceModel', component_property='figure'),
-     Output(component_id='ferModel', component_property='figure')],
-    [Input(component_id='controls-and-radio-item', component_property='value')]
+app.config['suppress_callback_exceptions'] = True
+
+@app.callback(
+    [
+        Output('deepfaceModel', 'figure'),
+        Output('ferModel', 'figure'),
+        Output('cnnModel', 'figure'),
+        Output('show-all-btn', 'n_clicks'),
+    ],
+    [
+        Input('show-all-btn', 'n_clicks')
+    ]
 )
+def update_graph(show_all_clicks):
+    emotion_chosen = ''
 
+    if show_all_clicks is None:
+        show_all_clicks = 0
 
-def update_graph(emotion_chosen):
-    colors = ['gold', 'blue', '#28fc03', 'red', '#000', 'purple', 'pink']
+    if show_all_clicks > 0:
+        emotion_chosen = ''
+        show_all_clicks -= 1
+
     fig = go.Figure()
     fig2 = go.Figure()
+    fig3 = go.Figure()
+
     if emotion_chosen == '':
-        for col in df.columns:
+        for col in column_names:
             fig.add_trace(go.Bar(
                 x=[col],
-                y=[df.loc[0][col]],
-                marker=dict(color=colors[2]),
+                y=[deepfaceCSV.loc[0][col]],
+                marker=dict(color="#00cc00"),
             ))
             fig2.add_trace(go.Bar(
                 x=[col],
-                y=[df.loc[0][col]],
-                marker=dict(color=colors[2]),
+                y=[ferCSV.loc[0][col]],
+                marker=dict(color="royalblue"),
             ))
-    else:
-        for col in df.columns:
-            if col == emotion_chosen:
-                fig.add_trace(go.Bar(
-                    x=[col],
-                    y=[df.loc[0][col]],
-                    marker=dict(color=colors[2]),
-                ))
-                fig2.add_trace(go.Bar(
-                    x=[col],
-                    y=[df.loc[0][col]],
-                    marker=dict(color=colors[2]),
-                ))
-            else:
-                fig.add_trace(go.Bar(
-                    x=[col],
-                    y=[0],
-                    marker=dict(color='rgba(0,0,0,0)'),
-                ))
-                fig2.add_trace(go.Bar(
-                    x=[col],
-                    y=[0],
-                    marker=dict(color='rgba(0,0,0,0)'),
-                ))
+            fig3.add_trace(go.Bar(
+                x=[col],
+                y=[cnnCSV.loc[0][col]],
+                marker=dict(color="gold"),
+            ))
 
+    for i, emotion in enumerate(column_names):
+        fig.data[i].name = emotion
+        fig2.data[i].name = emotion
+        fig3.data[i].name = emotion
 
     fig.update_layout(
         title='Deepface Model',
@@ -74,26 +78,70 @@ def update_graph(emotion_chosen):
         plot_bgcolor='#333',
         paper_bgcolor='#333',
         title_font=dict(color='white', size=20),
-        title_x=0.5
-
+        title_x=0.5,
+        legend=dict(
+            orientation='h',
+            x=0.5,
+            y=-0.3,
+            xanchor='center',
+            yanchor='top',
+            tracegroupgap=100,
+            itemsizing='constant',
+            itemwidth=80,
+            font=dict(color="white")
+        )
     )
     fig2.update_layout(
-        title='Deepface Model',
+        title='Fer Model',
         xaxis=dict(gridcolor='lightgray'),
         yaxis=dict(gridcolor='lightgray'),
         plot_bgcolor='#333',
         paper_bgcolor='#333',
         title_font=dict(color='white', size=20),
-        title_x=0.5
-
+        title_x=0.5,
+        legend=dict(
+            orientation='h',
+            x=0.5,
+            y=-0.3,
+            xanchor='center',
+            yanchor='top',
+            tracegroupgap=100,
+            itemsizing='constant',
+            itemwidth=80,
+            font=dict(color="white")
+        )
     )
+    fig3.update_layout(
+        title='CNN Model',
+        xaxis=dict(gridcolor='lightgray'),
+        yaxis=dict(gridcolor='lightgray'),
+        plot_bgcolor='#333',
+        paper_bgcolor='#333',
+        title_font=dict(color='white', size=20),
+        title_x=0.5,
+        legend=dict(
+            orientation='h',
+            x=0.5,
+            y=-0.3,
+            xanchor='center',
+            yanchor='top',
+            tracegroupgap=100,
+            itemsizing='constant',
+            itemwidth=80,
+            font=dict(color="white")
+        )
+    )
+
+    fig2.update_traces(marker=dict(color="royalblue"))
+    fig3.update_traces(marker=dict(color="gold"))
+
     fig.update_xaxes(title='Emotion', color='white')
     fig.update_yaxes(title='Percentage', color='white')
     fig2.update_xaxes(title='Emotion', color='white')
     fig2.update_yaxes(title='Percentage', color='white')
-    return fig, fig2
-
-
+    fig3.update_xaxes(title='Emotion', color='white')
+    fig3.update_yaxes(title='Percentage', color='white')
+    return fig, fig2, fig3, show_all_clicks
 
 if __name__ == '__main__':
     app.run_server(debug=True)
